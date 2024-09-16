2b17658847e9511aa243ac60d103d0cfabf62b24
Oluwadabira <adigunsamuelaso@gmail.com>
2024-09-16 16:23:52 +0100

    Register handler

--- internal/handlers/auth_handler.go
+++ internal/handlers/auth_handler.go
@@ -1,9 +1,35 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/Asonance11/Application-tracker/internal/models"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func Register(c *gin.Context) {
+	var user models.User
 
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to hash password"})
+		return
+	}
+
+	user.Password = string(hashedPassword)
+
+	if err := models.CreateUser(&user); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create user"})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"message": "User created successfully"})
 }
 
 func Login(c *gin.Context) {
