812363011125e220b63fd6722995787a4961bbfa
Oluwadabira <adigunsamuelaso@gmail.com>
2024-09-16 16:01:36 +0100

    Connecting to DB works

--- internal/models/job.go
+++ internal/models/job.go
@@ -40,9 +40,12 @@ type Job struct {
 
 // CreateJobStatusType creates the job_status enum type in the database
 func CreateJobStatusType(db *gorm.DB) error {
-	return db.Exec(`DO $$ BEGIN
-		CREATE TYPE job_status AS ENUM ('applied', 'got_response', 'got_interview', 'failed_interview', 'rejected', 'got_offer');
-	EXCEPTION
-		WHEN duplicate_object THEN null;
-	END $$;`).Error
+	return db.Exec(`
+        DO $$ 
+        BEGIN
+            CREATE TYPE job_status AS ENUM ('applied', 'got_response', 'got_interview', 'failed_interview', 'rejected', 'got_offer');
+        EXCEPTION
+            WHEN duplicate_object THEN null;
+        END $$;
+    `).Error
 }
