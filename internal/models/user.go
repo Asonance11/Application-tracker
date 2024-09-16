cadb4d7e7ed1ef443d68866974c31a302b4c45d5
Oluwadabira <adigunsamuelaso@gmail.com>
2024-09-16 16:27:02 +0100

    Add create user func

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"github.com/Asonance11/Application-tracker/internal/database"
 	"gorm.io/gorm"
 )
 
@@ -8,4 +9,12 @@ type User struct {
 	gorm.Model
 	Username string
 	Password string
+}
+
+func CreateUser(user *User) error {
+	if err := database.GetDB().Create(user).Error; err != nil {
+		return err
+	}
+
+	return nil
 }
