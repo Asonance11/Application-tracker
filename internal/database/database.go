b66187c7984e2d3a3fd20a238502a9827ce06ec7
Oluwadabira <adigunsamuelaso@gmail.com>
2024-09-23 11:29:52 +0100

    Fix db connection issue

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,6 +1,8 @@
 package database
 
 import (
+	"time"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -9,10 +11,24 @@ var db *gorm.DB
 
 func InitDB(dsn string) error {
 	var err error
-	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		PrepareStmt:            true,
+		SkipDefaultTransaction: true,
+	})
 	if err != nil {
 		return err
 	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		return err
+	}
+
+	// Set connection pool settings
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxOpenConns(100)
+	sqlDB.SetConnMaxLifetime(time.Hour)
+
 	return nil
 }
 
